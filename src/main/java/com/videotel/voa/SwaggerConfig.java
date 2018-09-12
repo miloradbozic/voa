43066ec3b768c329e662a76deae2e37671b00b00
Milorad Bozic <miloradbozic29@gmail.com>
2018-09-12 02:54:02 +0200

    add swagger

--- src/main/java/com/videotel/voa/SwaggerConfig.java
+++ src/main/java/com/videotel/voa/SwaggerConfig.java
@@ -14,8 +14,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
 
-//@Configuration
-//@EnableSwagger2
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig extends WebMvcConfigurationSupport {
     @Bean
     public Docket api() {
